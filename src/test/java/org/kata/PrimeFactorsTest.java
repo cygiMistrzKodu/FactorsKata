9e3a11adc0e53120e49119b78b11b112ae6f3d76
Jacek programmer <jacek301@gmail.com>
2024-02-16 21:50:35 +0100

    First Test na pass test. Add library

--- src/test/java/org/kata/PrimeFactorsTest.java
+++ src/test/java/org/kata/PrimeFactorsTest.java
@@ -1,14 +1,26 @@
 package org.kata;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class PrimeFactorsTest {
 
     @Test
     public void factors() {
 
-        fail();
+        assertThat(factorsOf(1), is(empty()));
 
+
+    }
+
+    private List<String> factorsOf(int n) {
+        return new ArrayList<>();
     }
 
 }
