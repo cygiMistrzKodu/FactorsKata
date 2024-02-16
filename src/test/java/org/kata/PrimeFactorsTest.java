2962fd3b65e69763455491956f57d1c70907b18b
Jacek programmer <jacek301@gmail.com>
2024-02-16 21:35:52 +0100

    FactorsKata Start

---
+++ src/test/java/org/kata/PrimeFactorsTest.java
@@ -0,0 +1,14 @@
+package org.kata;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class PrimeFactorsTest {
+
+    @Test
+    public void factors() {
+
+        fail();
+
+    }
+
+}
