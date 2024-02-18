4fa1fe8446746ac765e134eac9145a4c0effc2e6
Jacek programmer <jacek301@gmail.com>
2024-02-18 18:20:26 +0100

    Factor Kata training start

--- src/test/java/org/kata/PrimeFactorsTest.java
+++ src/test/java/org/kata/PrimeFactorsTest.java
@@ -8,39 +8,16 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class PrimeFactorsTest {
 
     @Test
     public void factors() {
 
-        assertThat(factorsOf(1), is(empty()));
-        assertThat(factorsOf(2), contains(2));
-        assertThat(factorsOf(3), contains(3));
-        assertThat(factorsOf(4), contains(2, 2));
-        assertThat(factorsOf(5), contains(5));
-        assertThat(factorsOf(6), contains(2, 3));
-        assertThat(factorsOf(7), contains(7));
-        assertThat(factorsOf(8), contains(2, 2, 2));
-        assertThat(factorsOf(9), contains(3, 3));
-
-
+        fail();
     }
 
-    private List<Integer> factorsOf(int n) {
 
-        ArrayList<Integer> factors = new ArrayList<>();
-        int divisor = 2;
-        while (n > 1) {
-            while (n % divisor == 0) {
-                factors.add(divisor);
-                n /= divisor;
-
-            }
-            divisor++;
-        }
-
-        return factors;
-    }
 
 }
